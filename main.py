b40a36f3a5875b3febbda091457350ed83f60a42
tinkybeatz <gaedelouis@gmail.com>
2025-01-03 01:53:48 +0100

    new try lol

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # Enable CORS for all routes
-CORS(app, origins=["http://localhost:4200", ["https://spotify-insights-ten.vercel.app"]])
+CORS(app, origins=["http://localhost:4200", "https://spotify-insights-ten.vercel.app"])
 
 @app.route('/')
 def home():
